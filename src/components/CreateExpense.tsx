b12ab27e74293843a3a35e08895c6a98b9996d4c
JDY <wjsehdud@nate.com>
2025-03-21 17:29:27 +0900

    feat: 지출 관리 컴포넌트 추가 (CreateExpense, ExpenseList, MouthNavigation)

---
+++ src/components/CreateExpense.tsx
@@ -0,0 +1,5 @@
+const CreateExpense = () => {
+  return <div>날짜,항목,금액,내용의 input들과 저장 button이 존재할 컴포넌트</div>;
+};
+
+export default CreateExpense;
---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,5 @@
+const ExpenseList = () => {
+  return <div>저장한 지출 내역들이 보이는 컴포넌트</div>;
+};
+
+export default ExpenseList;
